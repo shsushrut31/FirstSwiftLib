2c5b07bfe1e44b8c4cc73a9361b5d3fa66369ffc
Sushrut Shastri <shsushrut31@gmail.com>
2021-08-06 18:13:50 +0530

    Added test cases

---
+++ FirstSwiftLib/Addition.swift
@@ -0,0 +1,16 @@
+//
+//  Addition.swift
+//  FirstSwiftLib
+//
+//  Created by sushrut on 06/08/21.
+//
+
+import Foundation
+
+public final class Addition{
+    let name = "Addition"
+    
+    public func add(_ first:Int, to second: Int) -> Int{
+        return first + second
+    }
+}
--- FirstSwiftLibTests/FirstSwiftLibTests.swift
+++ FirstSwiftLibTests/FirstSwiftLibTests.swift
@@ -10,24 +10,14 @@ import XCTest
 
 class FirstSwiftLibTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var add: Addition!
+    
+    override func setUp() {
+        add = Addition()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testAdd() {
+        XCTAssertEqual(add.add(3, to: 5), 8)
     }
 
 }
