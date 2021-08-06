ee5be06803ac90fae87016b82d76106740a32e44
Sushrut Shastri <shsushrut31@gmail.com>
2021-08-06 17:53:19 +0530

    Initial project setup

---
+++ FirstSwiftLib/FirstSwiftLib.h
@@ -0,0 +1,18 @@
+//
+//  FirstSwiftLib.h
+//  FirstSwiftLib
+//
+//  Created by sushrut on 06/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirstSwiftLib.
+FOUNDATION_EXPORT double FirstSwiftLibVersionNumber;
+
+//! Project version string for FirstSwiftLib.
+FOUNDATION_EXPORT const unsigned char FirstSwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstSwiftLib/PublicHeader.h>
+
+
